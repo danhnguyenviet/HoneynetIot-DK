22ec4bad4ba8122e5da2618337e41f84ad91d2be
Danh Nguyen <danhnvuit@gmail.com>
2016-06-10 08:12:54 +0700

    Created HoneynetIot-DK resources

---
+++ HoneynetIotDK/app/src/androidTest/java/honeynet/com/honeynetiotdk/ApplicationTest.java
@@ -0,0 +1,13 @@
+package honeynet.com.honeynetiotdk;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ HoneynetIotDK/app/src/main/java/honeynet/com/honeynetiotdk/SettingsActivity.java
@@ -0,0 +1,13 @@
+package honeynet.com.honeynetiotdk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
